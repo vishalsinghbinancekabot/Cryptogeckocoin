2b4f40de2fbbe82aec2bb6eb97d8e378eb3333cd
vishalsinghbinancekabot <vishalsingh9899016@gmail.com>
2025-06-20 01:09:05 +0530

    Create trade_type.py

---
+++ trade_type.py
@@ -0,0 +1,12 @@
+📁 FILE: trade_type.py
+
+VishalX ProBot - Trade Type Detector
+
+def detect_trade_type(df): latest = df.iloc[-1] atr = latest['atr'] close = latest['close'] atr_percent = (atr / close) * 100
+
+if atr_percent < 1.2:
+    return "Scalping"
+elif atr_percent < 3:
+    return "Intraday"
+else:
+    return "Swing"
