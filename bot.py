e9ee94373f96b7969a6ea9ba790509d1e9f26916
vishalsinghbinancekabot <vishalsingh9899016@gmail.com>
2025-06-20 14:26:25 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,9 @@ import requests
 import re
 from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 
-#def escape_markdown(text):
-    escape_chars = r'\_*[]()~`>#+-=|{}.!%:'
+# ✅ MarkdownV2 escaping function
+def escape_markdown(text):
+    escape_chars = r'\_*[]()~`>#+-=|{}.!%:@\''
     return ''.join(['\\' + c if c in escape_chars else c for c in text])
 
 # ✅ Message sender
